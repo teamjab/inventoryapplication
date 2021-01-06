dbe44b3d2274d6d4feefb1b8488bb0c8401ae88e
BrendonLH <Brendonhampton1988@gmail.com>
2021-01-06 10:49:45 -0800

    added some logo changes

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -11,33 +11,6 @@ function editUpdate(e){
 $('.tableRow').hide();
 
 
-
-////brendon's code////
-
-// array to store items 
-// let items = [];
-// // function to push input values into array
-// const addItem = (ev)=>{
-//   ev.preventDefault(); //stops the form from submitting
-//   let item = {
-//     Name: document.getElementById('item').value,
-//     PO: document.getElementById('po').value,
-//     Lot: document.getElementById('lot').value,
-//     Received: document.getElementById('received').value,
-//     Expiration: document.getElementById('expiration').value,
-//     Quantity: document.getElementById('quantity').value,
-//     type: document.querySelector('input[name="type"]:checked').value
-//   }
-//   items.push(item);
-//   //clears form for another entry
-//   document.querySelector('form').reset();
-//   console.log(items); 
-// }
-// //event listener for button
-// document.addEventListener('DOMContentLoaded', ()=>{
-//   document.getElementById('submit').addEventListener('click', addItem);
-// });
-
 // search function
 function search(){
   // element selectors
